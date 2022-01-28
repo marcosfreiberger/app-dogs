ab0c0a5eef37596396dcd48c4f970c33888300c4
Freiberger <marcos.freiberger@capgemini.com>
2022-01-28 11:14:35 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+<h1 align = "center"> :fast_forward: Dogs :rewind: </h1>
+
+Para visualizar o projeto acesse:
+
+## 📖 Sobre
+
+<p>Site Dogs desenvolvido no curso de React Completo - Origamid.</p>
+
+---
+
+## 🛠 Tecnologias utilizadas
+
+Frontend
+
+- HTML
+- CSS
+- JavaScript
+- React
+
+---
+
+Developed with 💙 by Marcos N. Freiberger
---
+++ src/Components/Login/LoginForm.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Button from '../Forms/Button';
+import Input from '../Forms/Input';
+import useForm from '../../Hooks/useForm';
+import { UserContext } from './../../UserContext';
+import Error from '../Helper/Error';
+import styles from './LoginForm.module.css';
+import stylesBtn from '../Forms/Button.module.css';
+import Head from '../Helper/Head';
+
+const LoginForm = () => {
+  const username = useForm();
+  const password = useForm();
+
+  const { userLogin, error, loading } = React.useContext(UserContext);
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    if (username.validate() && password.validate()) {
+      userLogin(username.value, password.value);
+    }
+  }
+
+  return (
+    <section className="animeLeft">
+      <Head title="Login" />
+      <h1 className="title">Login</h1>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <Input label="Usuário" type="text" name="username" {...username} />
+        <Input label="Senha" type="password" name="password" {...password} />
+        {loading ? (
+          <Button disabled>Carregando...</Button>
+        ) : (
+          <Button>Entrar</Button>
+        )}
+        <Error error={error && 'Dados incorretos.'} />
+      </form>
+      <Link to="/login/perdeu" className={styles.perdeu}>
+        Perdeu a senha?
+      </Link>
+      <div className={styles.cadastro}>
+        <h2 className={styles.subtitle}>Cadastre-se</h2>
+        <p>Ainda não possui conta? Cadastre-se no site.</p>
+        <Link to="/login/criar" className={stylesBtn.button}>
+          Cadastro
+        </Link>
+      </div>
+    </section>
+  );
+};
+
+export default LoginForm;
